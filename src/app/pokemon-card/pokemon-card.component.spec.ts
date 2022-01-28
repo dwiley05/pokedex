108e03ad330aa1b774d0ed99a799bc73d53e6114
dwiley05 <wiled5@gmail.com>
2022-01-27 21:57:40 -0700

    unit testing

--- src/app/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/pokemon-card/pokemon-card.component.spec.ts
@@ -2,14 +2,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { Pokemon } from '../interfaces/pokemon';
 
 import { PokemonCardComponent } from './pokemon-card.component';
 
 describe('PokemonCardComponent', () => {
   let component: PokemonCardComponent;
   let fixture: ComponentFixture<PokemonCardComponent>;
+  let pokemonStore;
+  let pokemon: Partial<Pokemon> = {name: 'pikachu', id: 1 };
 
   beforeEach(async () => {
+     pokemonStore = { 
+      getSinglePokemon: of(pokemon)
+    };
+
     await TestBed.configureTestingModule({
       declarations: [ PokemonCardComponent ],
       providers: [HttpClientModule],
